8ce72df88081d1beab18931295409353b6bb1963
Leo Zhu <awesome.zhu@outlook.com>
2015-11-27 14:39:31 +0800

    Add Data tracking script

---
+++ zh/_Scripts/ActionTail.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Leo on 2015/11/27.
+ */
+
+$(document).ready(function () {
+
+    //Track Register
+    $('#element').click(function () {
+        ga('send', 'event', 'button', 'click', 'register');
+    });
+
+});
