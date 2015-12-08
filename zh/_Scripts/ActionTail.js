a743bead979003e0a6fd5f120afed86073fb7ad3
Leo Zhu <awesome.zhu@outlook.com>
2015-12-08 18:06:26 +0800

    update Data tracking script

--- zh/_Scripts/ActionTail.js
+++ zh/_Scripts/ActionTail.js
@@ -4,11 +4,6 @@
 
 $(document).ready(function () {
 
-    //Track Registration
-    $('#element').click(function () {
-        ga('send', 'event', 'button', 'click', 'register');
-    });
-
     var pathName=window.location.pathname;
     var registerPathName = /^https:\/\/maxleap.cn\/regnotify\?send_email_flag=success\&verify_email_address=[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
     if(pathName.match(registerPathName)  ) {
