0d51b9a731aa2343334b775a3b084f90b4b05965
Leo Zhu <awesome.zhu@outlook.com>
2015-12-08 18:21:43 +0800

    update Data tracking script

--- zh/_Scripts/ActionTail.js
+++ zh/_Scripts/ActionTail.js
@@ -5,9 +5,14 @@
 $(document).ready(function () {
 
     var pathName=window.location.pathname;
-    var registerPathName = /^https:\/\/maxleap.cn\/regnotify\?send_email_flag=success\&verify_email_address=[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    if(pathName.match(registerPathName)  ) {
-        ga('send', 'event', 'Activation', 'RegisterDone', 'Succeed');
+    var registerPathName = /^\/regnotify\?send_email_flag=success\&verify_email_address=[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    if(pathName.test(registerPathName)  ) {
+        ga('send', 'event', 'Activation', 'Register', 'Done');
+    }
+
+    var homePathName = /^\/zh_cn\/index\.html$/;
+    if(pathName.test(homePathName)  ) {
+        ga('send', 'event', 'Acquisition', 'HomeVisit', 'Enter');
     }
 
 });
